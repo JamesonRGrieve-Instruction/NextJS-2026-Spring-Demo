e542b863de66fe949e862bba678facc3d0a2b9dc
Jameson Grieve <jamesonrgrieve@gmail.com>
2026-04-15 19:28:04 -0600

    Query params;

--- app/example/[id]/page.js
+++ app/example/[id]/page.js
@@ -1,9 +1,9 @@
 import React from "react";
-export default function Dynamic({params}) {
+export default function Dynamic({params, searchParams}) {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Dynamic Page - {React.use(params).id}</h1>
+        <h1>Dynamic Page - {React.use(params).id} for {React.use(searchParams).name} at {React.use(searchParams).time}</h1>
       </main>
     </div>
   );
