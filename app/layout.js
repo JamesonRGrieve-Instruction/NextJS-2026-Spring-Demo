407882d57d8a4899804cba4d12a985d2701a6a06
Jameson Grieve <jamesonrgrieve@gmail.com>
2026-04-23 19:04:22 -0600

    Demo;

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono  } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.variable} ${robotoMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <nav>
