691b1497b3ea9530f33f8bda994dc66b896371f8
Jameson Grieve <jamesonrgrieve@gmail.com>
2026-04-29 19:25:20 -0600

    This *would* have caused a hydration error, had Vercel not fixed almost all of the causes;

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,18 +1,20 @@
-import React from "react";
+"use client"
+import React, {useState} from "react";
 
-export async function generateStaticParams() {
-  return [1,2,3,4,5,6,7,8,9,10].map((id) => ({
-    id: id.toString()
-  }));
-}
-export const dynamicParams = false;
+// export async function generateStaticParams() {
+//   return [1,2,3,4,5,6,7,8,9,10].map((id) => ({
+//     id: id.toString()
+//   }));
+// }
+// export const dynamicParams = false;
 
 
-export default async function Home({params}) {
-  const { id } = await params;
+export default  function Home({params}) {
+
+  const random = Math.floor(Math.random() * 100);
   return (
       <main>
-        <p>{id}</p>
+        <p>{random}</p>
       </main>
   );
 }
