fb7783538d56db5e80f5b1cb800aad8b60a50c2b
Jameson Grieve <jamesonrgrieve@gmail.com>
2026-05-06 19:24:15 -0600

    Cache keys;

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, {useActionState, useState} from "react";
-import useSWR from "swr";
+import useSWR, {mutate} from "swr";
 import { doAThing } from "@/app/actions";
 // export async function generateStaticParams() {
 //   return [1,2,3,4,5,6,7,8,9,10].map((id) => ({
@@ -14,13 +14,18 @@ export default  function Home({params}) {
   const [state, formAction, isPending] = useActionState(doAThing, null);
   const { data, isLoading, error } = useSWR('joke', async () => {
     return await (await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=racist,sexist&type=single")).json();
+  }, {
+    fallbackData: {
+      joke: "Knock knock..."
+    }
   })
   console.log(data);
   return (
       <main>
-        <p>{isLoading ? "Joke is loading..." : data.joke}</p>
+        <p>{data.joke}</p>
         <h1>State</h1>
         <p>{state?.success ?? "Nothing"} {isPending ? "Yes" : "No"}</p>
+        <button onClick={() => mutate("joke")}>New Joke</button>
         <form action={formAction}>
           <button type="submit">Submit</button>
         </form>
