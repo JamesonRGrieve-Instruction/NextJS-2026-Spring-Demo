ffcd11365068f15fc2246cb27da45ab72d8821d9
Jameson Grieve <jamesonrgrieve@gmail.com>
2026-05-06 19:11:01 -0600

    Basic SWR syntax;

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,5 +1,6 @@
 "use client"
 import React, {useActionState, useState} from "react";
+import useSWR from "swr";
 import { doAThing } from "@/app/actions";
 // export async function generateStaticParams() {
 //   return [1,2,3,4,5,6,7,8,9,10].map((id) => ({
@@ -11,10 +12,13 @@ import { doAThing } from "@/app/actions";
 
 export default  function Home({params}) {
   const [state, formAction, isPending] = useActionState(doAThing, null);
-  const random = Math.floor(Math.random() * 100);
+  const { data, isLoading, error } = useSWR('joke', async () => {
+    return await (await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=racist,sexist&type=single")).json();
+  })
+  console.log(data);
   return (
       <main>
-        <p>{random}</p>
+        <p>{isLoading ? "Joke is loading..." : data.joke}</p>
         <h1>State</h1>
         <p>{state?.success ?? "Nothing"} {isPending ? "Yes" : "No"}</p>
         <form action={formAction}>
